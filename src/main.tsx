817b8cca07e468b04ef3a6806f2ce20621ead8db
Luis Arancibia <l.arancibiaf@gmail.com>
2022-09-20 17:49:59 -0300

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@material-tailwind/react'
+import { Provider } from 'react-redux'
+import { store } from '@/app/store'
 
 import App from './App'
 import './index.css'
@@ -9,9 +11,11 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/Landing/Landing.page.tsx
+++ src/pages/Landing/Landing.page.tsx
@@ -1,7 +1,27 @@
+import { useAppDispatch, useAppSelector } from '@/app/hooks'
+import { amountAdded, incremented } from '@/features/counter/counter.slice'
+
 import './Landing.page.scss'
 
-export default () => (
-  <section className="w-full h-full">
-    <h1>landing</h1>
-  </section>
-)
+export default () => {
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  function handleClick() {
+    dispatch(incremented())
+  }
+
+  function handleClickByThree() {
+    dispatch(amountAdded(3))
+  }
+
+  return (
+    <section className="w-full h-full">
+      <h1>landing</h1>
+      <h2>count is {count}</h2>
+
+      <button onClick={handleClick}>+++++</button>
+      <button onClick={handleClickByThree}>+3</button>
+    </section>
+  )
+}
